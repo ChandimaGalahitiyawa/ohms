75e32db201d065f753edc549f5ca1bad94409591
Chandima Galahitiyawa <bizchandima@gmail.com>
2024-05-12 10:42:45 +0530

    update

--- config/stripe.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'stripe_publishable_key' => env('STRIPE_PUBLISHABLE_KEY'),
-    'stripe_secret_key' => env('STRIPE_SECRET_KEY'),
-];
--- resources/views/dashboard.blade.php
+++
@@ -1,6 +0,0 @@
-{{-- sorry this page is not available --}}
-<script>
-  document.addEventListener('DOMContentLoaded', function() {
-      window.location.href = "{{ route('auto.logout') }}"; // Redirects to the logout route automatically
-  });
-  </script>
